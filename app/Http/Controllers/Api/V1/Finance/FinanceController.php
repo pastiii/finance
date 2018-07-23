2898384cf86f1ced282c2a4abf9dc8963dc0bc90
ghzhl <m15202471353@163.com>
2018-07-23 17:26:31 +0800

    finance

--- app/Http/Controllers/Api/V1/Finance/FinanceController.php
+++ app/Http/Controllers/Api/V1/Finance/FinanceController.php
@@ -10,12 +10,16 @@ namespace App\Http\Controllers\Api\V1\Finance;
 use App\Http\Controllers\Api\V1\BaseController;
 use Dingo\Api\Http\Request;
 use App\Services\FinanceService;
+use App\Services\UserService;
+use App\Support\SaltTrait;
 
 class FinanceController extends BaseController
 {
+     use SaltTrait;
      /* @var FinanceService  $financeService*/
-
      protected $financeService;
+    /* @var UserService  $userService*/
+     protected $userService;
 
      public function __construct()
      {
@@ -33,6 +37,48 @@ class FinanceController extends BaseController
         return $this->financeService;
      }
 
+     protected  function getUserService()
+     {
+        if (!isset($this->userService)) {
+            $this->userService = app(UserService::class);
+        }
+        return $this->userService;
+
+     }
+
+    /**
+     * 获取用户基本信息
+     * @return array
+     */
+    public function getUserInfo()
+    {
+        //获取用户创建时间
+        $this->getUserService();
+        $user=$this->get_user_info();
+
+        $user_info = $this->userService->getUser($this->user_id);
+        if ($user_info['code'] != 200) {
+            $code  = $this->code_num('GetUserFail');
+            return $this->errors($code, __LINE__);
+        }
+        //获取用户最后一次登录历史
+        $page       = 1;
+        $pageSize   = 1;
+        $last_login = $this->userService->getUserLoginHistoryList($this->user_id, $pageSize, $page);
+        //账号信息获取
+        $email = $this->userService->getUserEmailById($this->user_id);
+        $phone = $this->userService->getUserPhone($this->user_id);
+        //数据处理
+        $data['email']            = empty($email['data']) ? "" : substr($email['data']['email'], '0', 3)."*****".strstr($email['data']['email'], "@",false);
+        $data['phone']            = empty($phone['data']) ? "" : substr($phone['data']['phone_number'] , 0 , 3)."******".substr($phone['data']['phone_number'], -2,2);
+        $data['name']             = $user['user_name'];
+        $data['last_login_time']  = isset($last_login['data']['list']) ? date('Y-m-d H:i:s', $last_login['data']['list'][0]['created_at']) : '';
+        $data['finance_usdt']= 0;
+        $data['finance_rmb']=0;
+        $data['finance_us']=0;
+        return $this->response($data, 200);
+    }
+
      /**
      * 获取用户资产信息列表
      * @param Request $request
@@ -41,7 +87,6 @@ class FinanceController extends BaseController
      public function getFinanceList(Request $request)
      {
          $this->getFinanceService();
-
          $data=$this->validate($request, [
              'limit'   => 'required|int|min:1',
              'page'    => 'required|int|min:1',
@@ -56,7 +101,7 @@ class FinanceController extends BaseController
              return $this->errors($code,__LINE__);
          }
 
-         return $this->response($list['data'], 200);;
+         return $this->response($list['data'], 200);
      }
 
      /**
@@ -64,22 +109,35 @@ class FinanceController extends BaseController
      * @param Request $request
      * @return array
      */
-     public function getwalletAddr(Request $request)
+     public function getFinanceWallet(Request $request)
      {
          $this->getFinanceService();
 
          $data=$this->validate($request, [
-             'finance_wallet_id' => 'required|int|min:1'
+             'finance_id' => 'required|int|min:1'
          ]);
 
-         $addr=$this->financeService->getwalletAddr($data);
+         $finance_info=$this->financeService->getFinance($data['finance_id']);
+         if(empty($finance_info['data'])){
+             $code=$this->code_num('FinanceEmpty');
+             return $this->errors($code,__LINE__);
+         }
+         $param= [
+             "user_id"  =>$this->user_id,
+             "user_name"=>$finance_info['data']['user_name'],
+             "coin_id"  =>$finance_info['data']['coin_id'],
+             "coin_name"=>$finance_info['data']['coin_name'],
+             "coin_type"=>$finance_info['data']['coin_type']
+         ];
+         $info=$this->financeService->createFinanceWallet($param);
 
-         if(empty($addr['data'])){
+         if(empty($info['data'])){
              $code=$this->code_num('Empty');
              return $this->errors($code,__LINE__);
          }
-
-         return $this->response($addr['data'], 200);
+         $res['wallet_addr']=$info['data']['wallet_addr'];
+         $res['qr_msg']=base64_encode($info['data']['wallet_addr']);
+         return $this->response($res, 200);
      }
 
      /**
@@ -135,6 +193,85 @@ class FinanceController extends BaseController
          return $this->response($info['data'], 200);
          
      }
+
+     /**
+     * 提交提现申请
+     * @param Request $request
+     * @return array
+     */
+     public function createFinanceWithdraw(Request $request)
+     {
+         $this->getFinanceService();
+
+         $data=$this->validate($request, [
+             'finance_id' => 'required|int|min:1',
+             'destination_addr' => 'required|string',//目标地址
+             'withdraw_amount' => 'required|string',
+             'password' =>'required'
+         ]);
+         //钱包信息
+         $finance_info=$this->financeService->getFinance($data['finance_id']);
+         if(empty($finance_info['data'])){
+             $code=$this->code_num('FinanceEmpty');
+             return $this->errors($code,__LINE__);
+         }
+         //判断余额
+         if($data['withdraw_amount'] > $finance_info['data']['finance_available']){
+             $code=$this->code_num('FinanceAvailable');
+             return $this->errors($code,__LINE__);
+         }
+         //检查password
+         $pin_code=$this->checkPin($data['password']);
+         if($pin_code !== true){
+             return $this->errors($pin_code,__LINE__);
+         }
+
+         //组装数据
+         $withdraw_data=[
+             'finance_id'  =>$finance_info['data']['finance_id'],
+             'user_id'     =>$finance_info['data']['user_id'],
+             'user_name'   =>$finance_info['data']['user_name'],
+             'coin_id'     =>$finance_info['data']['coin_id'],
+             'coin_name'   =>$finance_info['data']['coin_name'],
+             'coin_type'   =>$finance_info['data']['coin_type'],
+             'destination_addr'        =>$data['destination_addr'],
+             'finance_withdraw_amount' =>intval($data['withdraw_amount']),
+             'finance_withdraw_amount_str' =>$data['withdraw_amount'],
+             'finance_withdraw_status' =>1
+         ];
+         //创建
+         $res=$this->financeService->createFinanceWithdraw($withdraw_data);
+
+         if($res['code'] != 200){
+             $code=$this->code_num('CreateFailure');
+             return $this->errors($code,__LINE__);
+         }
+
+         return $this->response($res['data'], 200);
+     }
+
+    /**
+     * 验证资金密码
+     * @param $pin string
+     * @return int
+     */
+    public function checkPin($pin){
+        $this->getUserService();
+        $ping_data = $this->userService->getUserPin($this->get_user_info());
+
+        if($ping_data['code'] !=200 || empty($ping_data['data'])){
+            $code = $this->code_num('GetPinFail');
+            return $code;
+        }
+        //验证密码
+        $password = $this->checkPassword($pin,$ping_data['data']['pin'],$ping_data['data']['salt']);
+        //判断密码是否正确
+        if(!$password){
+            $code = $this->code_num('PinError');
+            return $code;
+        }
+        return true;
+    }
 
 
 }
